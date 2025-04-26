c545ca918c75027642c175fef8f0e5753ff96237
Nazars Naumovs <naumovsnazars@gmail.com>
2025-04-26 14:50:38 -0400

    define project structure

---
+++ WTWR-app/src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "../Header/Header";
+
+function App() {
+  return (
+    <>
+      <div className="page">
+        <div className="page__content">
+          <Header />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ WTWR-app/src/components/Header/Header.jsx
@@ -0,0 +1,34 @@
+import "./Header.css";
+import headerLogo from "../../images/wtwr°.svg";
+import userPicture from "../../images/terrence_pfp.svg";
+
+function Header() {
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
+
+  return (
+    <>
+      <div className="header">
+        <div className="header__group">
+          <img className="header__logo" src={headerLogo} alt="WTWR logo" />
+          <p className="header__text">{currentDate}, Bel Air</p>
+        </div>
+        <div className="header__group">
+          <button className="header__add-btn">+ Add clothes</button>
+          <div className="header__user-info">
+            <p className="header__text">Nazars Naumovs</p>
+            <img
+              className="header__userpic"
+              src={userPicture}
+              alt="User's profile picture"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
